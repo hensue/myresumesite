943fbebde6b3d4d591719b78f572b1e71e9b720b
Lim Kim San <gentleman97626@gmail.com>
2023-11-13 10:07:02 -0800

    this is third

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -33,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Yassin</footer>
+          <footer className="blockquote-footer">Lim Kim San</footer>
         </blockquote>
       </Card.Body>
     </Card>
